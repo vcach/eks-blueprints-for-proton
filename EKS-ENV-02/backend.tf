67d19e0619e236075a16f0186f196fe35f169a27
sbx_user1051 <sbx_user1051@169.254.8.153>
2022-09-22 21:45:02 +0000

    Update requested for Proton - environment/EKS-ENV-02

---
+++ EKS-ENV-02/backend.tf
@@ -0,0 +1,11 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:us-east-2:345768074908:environment/EKS-ENV-02
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+terraform {
+  backend "s3" {}
+}
